7d4d0d62f74b1216992ca3cc41fe64e5ce641648
Michael Páris <mikepax@gmail.com>
2021-08-08 01:47:40 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 3

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 
 MAX_WAIT = 5
